26eca6bc6bc0b19b2610f79d23b8e88a3dd13f10
Neha Singh <neha.s@zycus.com>
2021-02-21 12:06:11 +0530

    TestCase

--- src/app/listing-section/listing-section.component.spec.ts
+++ src/app/listing-section/listing-section.component.spec.ts
@@ -23,22 +23,12 @@ describe('ListingSectionComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should test the table ', (done) => {
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should test the table ', () => {
     expect(component.dataSource[0]['firstname']).toBe('neha');
-    fixture.detectChanges();
-    // fixture.whenStable().then(() => {
-    //   const tableRows = fixture.debugElement.query(By.css('tr')).nativeElement;
-    //   expect(tableRows.length).toBe(4);
-    //   // Header row
-    //   let headerRow = tableRows[0];
-    //   expect(headerRow.cells[0].innerHTML).toBe('firstname');
-    //   expect(headerRow.cells[1].innerHTML).toBe('lastname');
-    //   // Data rows
-    //   let row1 = tableRows[1];
-    //   expect(row1.cells[0].innerHTML).toBe('neha');
-    //   expect(row1.cells[1].innerHTML).toBe('singh');
-    //   done();
-    // });
   });
   
 });
