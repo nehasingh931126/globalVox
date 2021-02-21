241e3c14a5c3b6a0b1371ecb61459d00ead5b1c9
Neha Singh <neha.s@zycus.com>
2021-02-21 11:49:47 +0530

    Test

--- src/app/listing-section/listing-section.component.spec.ts
+++ src/app/listing-section/listing-section.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ListingSectionComponent } from './listing-section.component';
 
 describe('ListingSectionComponent', () => {
@@ -21,4 +22,23 @@ describe('ListingSectionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should test the table ', (done) => {
+    expect(component.dataSource[0]['firstname']).toBe('neha');
+    fixture.detectChanges();
+    // fixture.whenStable().then(() => {
+    //   const tableRows = fixture.debugElement.query(By.css('tr')).nativeElement;
+    //   expect(tableRows.length).toBe(4);
+    //   // Header row
+    //   let headerRow = tableRows[0];
+    //   expect(headerRow.cells[0].innerHTML).toBe('firstname');
+    //   expect(headerRow.cells[1].innerHTML).toBe('lastname');
+    //   // Data rows
+    //   let row1 = tableRows[1];
+    //   expect(row1.cells[0].innerHTML).toBe('neha');
+    //   expect(row1.cells[1].innerHTML).toBe('singh');
+    //   done();
+    // });
+  });
+  
 });
